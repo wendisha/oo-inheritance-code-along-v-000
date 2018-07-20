4a748dddd1bd2edfd947ffbc072ba796e6f2e5a4
Wendy Calderon <wendycalderon@gmail.com>
2018-07-20 01:25:27 +0000

    automatically backed up by learn

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,10 @@
 require_relative "./vehicle.rb"
+
+class Car 
+  attr_accessor :wheel_size, :wheel_number 
+  
+  def initialize(wheel_size, wheel_number)
+    @wheel_size = wheel_size 
+    @wheel_number = wheel_number
+  end
+end
